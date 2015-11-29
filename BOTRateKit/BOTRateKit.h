4fa740325d5019c2ec5fdee69d7009db613a1d68
Azyukei <Azyukei@azyukei-macbook-pro.local>
2015-11-29 20:21:30 +0800

    Initial Commit

---
+++ BOTRateKit/BOTRateKit.h
@@ -0,0 +1,19 @@
+//
+//  BOTRateKit.h
+//  BOTRateKit
+//
+//  Created by Azyukei on 2015/11/29.
+//  Copyright © 2015年 Azyukei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BOTRateKit.
+FOUNDATION_EXPORT double BOTRateKitVersionNumber;
+
+//! Project version string for BOTRateKit.
+FOUNDATION_EXPORT const unsigned char BOTRateKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BOTRateKit/PublicHeader.h>
+
+
